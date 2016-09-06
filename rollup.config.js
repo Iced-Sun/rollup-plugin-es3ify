71ab24519d657a900089eff6ecede185faecae7f
Bing Sun <subi.the.dream.walker@gmail.com>
2016-09-06 21:08:03 +0800

    drop dead code

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,7 @@ import buble from 'rollup-plugin-buble';
 var external = Object.keys( require( './package.json' ).dependencies ).concat( 'path' );
 
 export default {
-	entry: 'src/index.js',
-	plugins: [ buble()],
+	entry: 'index.js',
+	plugins: [ buble() ],
 	external: external
 };
-
-/*
-export default {
-	entry: 'index.js',
-	plugins: [buble()],
-	targets: [
-		{
-			format: 'cjs',
-			dest: pkg['main']
-		},
-		{
-			format: 'es6',
-			dest: pkg['jsnext:main']
-		}
-	]
-};
- */
